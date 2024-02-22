f76f6db470fb8f5b889afc5756c75a00e59ca7f1
Andreas Kwong <andreaskwong@gmail.com>
2024-02-22 16:48:42 +0000

    Completed Absence Recorder 2

---
+++ Absence Recorder/Extensions/Date.swift
@@ -0,0 +1,8 @@
+//
+//  Date.swift
+//  Absence Recorder
+//
+//  Created by Andreas Kwong on 22/02/2024.
+//
+
+import Foundation
---
+++ Absence Recorder/View/DivisionItem.swift
@@ -0,0 +1,18 @@
+//
+//  DivisionItem.swift
+//  Absence Recorder
+//
+//  Created by Andreas Kwong on 22/02/2024.
+//
+
+import SwiftUI
+
+struct DivisionItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DivisionItem()
+}
