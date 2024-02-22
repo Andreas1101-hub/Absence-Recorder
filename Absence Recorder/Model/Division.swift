288b9b9c21a85858660b5b14068a85abec9cf225
Andreas Kwong <andreaskwong@gmail.com>
2024-02-22 16:08:15 +0000

    Completed Absence Recorder 1

---
+++ Absence Recorder/Model/Division.swift
@@ -0,0 +1,8 @@
+//
+//  Division.swift
+//  Absence Recorder
+//
+//  Created by Andreas Kwong on 22/02/2024.
+//
+
+import Foundation
---
+++ Absence Recorder/Model/Student.swift
@@ -0,0 +1,8 @@
+//
+//  Student.swift
+//  Absence Recorder
+//
+//  Created by Andreas Kwong on 22/02/2024.
+//
+
+import Foundation
